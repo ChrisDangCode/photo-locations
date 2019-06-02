5a95d835ed8d5f9bb1d1cfd82d4ad9405277e62b
Chris Dang <cdangcode@gmail.com>
2019-06-01 19:54:49 -0500

    Added markers for majority of locations
    Some locations do not have very accurate location names so I can't find all of them

--- mapbox.js
+++ mapbox.js
@@ -1,13 +1,297 @@
 /* const mapboxgl = require('mapbox-gl'); */
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2Rhbmdjb2RlIiwiYSI6ImNqd2I1Mnc2MjBoMzk0YW9kZGt3cnZuaWQifQ.JzdvfTUTtVOQBOpXWC-uXg'
 
-var map = new mapboxgl.Map({
-  container: 'map', // HTML container id
-  style: 'mapbox://styles/mapbox/streets-v9', // style URL
-  center: [-21.9270884, 64.1436456], // starting position as [lng, lat]
-  zoom: 13
-});
+        mapboxgl.accessToken = 'pk.eyJ1IjoiY2Rhbmdjb2RlIiwiYSI6ImNqd2I1Mnc2MjBoMzk0YW9kZGt3cnZuaWQifQ.JzdvfTUTtVOQBOpXWC-uXg';
+        var map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        center: [-93.232583, 44.974139], // starting position [lng, lat]
+        zoom: 13 // starting zoom
+        });
 
+
+/* Keller Hall - Center */
+/*
 var marker = new mapboxgl.Marker()
-.setLngLat([-21.9270884, 64.1436456])
+.setLngLat([-93.232583, 44.974139])
+.addTo(map);
+*/
+
+/* 1 Comstock */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236813, 44.972402])
+.addTo(map);
+
+/* 2 Acadia */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.246970, 44.970236,])
+.addTo(map);
+
+/* 3 Comstock */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236813, 44.972402])
+.addTo(map);
+
+/* 4 Burrito Loco */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236547, 44.981525])
+.addTo(map);
+
+/* 5 Comstock *again* */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236813, 44.972402])
+.addTo(map);
+
+/* 6 Blarney Pub and Grill */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.235220, 44.980822])
+.addTo(map);
+
+/* 7 Mesa Pizza */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236112, 44.980865])
+.addTo(map);
+
+/* 8 McDonalds */
+var marker = new mapboxgl.Marker()
+.setLngLat([ -93.234485, 44.980220])
+.addTo(map);
+
+/* 9 Washington Bridge */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.239781, 44.973094])
+.addTo(map);
+
+/* 10 Cattle Teaching and Research Building */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.176754, 44.985626])
+.addTo(map);
+
+/* 11 St. Paul Gym */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.186593, 44.987781])
+.addTo(map);
+
+/* 12 Al's Breakfast */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.235644, 44.981047])
+.addTo(map);
+
+/* 13 approx St Paul Campus */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.186583, 44.987778])
+.addTo(map);
+
+/* 14 Washington Ave Ave */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.231737, 44.973592])
+.addTo(map);
+
+/* 15 Cooke Hall Ave */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.230439, 44.976009])
+.addTo(map);
+
+/* 16 Nils Hasselmo Hall */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.234231, 44.972855])
+.addTo(map);
+
+/* 17 Folwell Hall */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.234282, 44.978193])
+.addTo(map);
+
+/* 18 Fast Eddies */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236588, 44.980475])
+.addTo(map);
+
+/* 19 Cooke Hall Ave */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.230439, 44.976009])
+.addTo(map);
+
+/* 20 The Knoll */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236390, 44.978584])
+.addTo(map);
+
+
+/* 21 Rarig Center */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.242402, 44.970315])
+.addTo(map);
+
+/* 22 Washington and Church */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.233835, 44.975991])
+.addTo(map);
+
+/* 23 Front of Northrop */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.235389, 44.972402])
+.addTo(map);
+
+/* 24 West Bank Common Push 4 art */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.243493, 44.972991])
+.addTo(map);
+
+/* 25 Coffman */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.235287, 44.973069])
+.addTo(map);
+
+/* 26 Tate */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.234678, 44.975129])
+.addTo(map);
+
+/* 27 West Bank studio */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.242550, 44.970506])
+.addTo(map);
+
+/* 28 Magus Books and Herbs */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236539, 44.980985])
+.addTo(map);
+
+/* 29 Delta Kappa Epsilon */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.231746, 44.977732])
+.addTo(map);
+
+/* 30 Dinkytown Tattoo */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.235845, 44.980807])
+.addTo(map);
+
+/* 31 Pi Kappa Alpha */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.230444, 44.977276])
+.addTo(map);
+
+/* 32 Dinkytown */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.235291, 44.980310])
+.addTo(map);
+
+/* 33 Williams Arena */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.228005, 44.976816])
+.addTo(map);
+
+/* 34 The Knoll */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236390, 44.978584])
+.addTo(map);
+
+/* 35 Ralph Rapson Hall */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.233190, 44.976285])
+.addTo(map);
+
+/* 36 Mississippi River */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.235265, 44.970019])
+.addTo(map);
+
+/* 37 The Knoll */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236390, 44.978584])
+.addTo(map);
+
+/* 38 Jane Sage Cowles Stadium */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.229101, 44.981275])
+.addTo(map);
+
+/* 39 Water area */
+/*
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236813, 44.972402])
+.addTo(map);
+*/
+
+/* 40 Rec Wellness */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.230095, 44.975484])
+.addTo(map);
+
+/* 41 Rec Wellness */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.230095, 44.975484])
+.addTo(map);
+
+/* 42 St Paul Center */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.185268, 44.984862])
+.addTo(map);
+
+/* 43 Ferguson Hall */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.241754, 44.970653])
+.addTo(map);
+
+
+/* 44 Judo Hall */
+/*
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236813, 44.972402])
+.addTo(map);
+*/
+
+/* 45 Rec Wellness */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.230095, 44.975484])
+.addTo(map);
+
+/* 46 Uni Aquatic center */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.229479, 44.975894])
+.addTo(map);
+
+/* 47 Whole Music club */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.235133, 44.972631])
+.addTo(map);
+
+/* 48 Bell museum */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.187972, 44.991161])
+.addTo(map);
+
+/* 49 Varsity Theatre */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236814, 44.980736])
+.addTo(map);
+
+/* 50 Insomia Cookies */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.235530, 44.980630])
+.addTo(map);
+
+/* 51 Stadium village tracks */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.227955, 44.973759])
+.addTo(map);
+
+/* 52 Kitty Kat Club */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236455, 44.980044])
+.addTo(map);
+
+
+/* 53 Como area */
+/*
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236813, 44.972402])
+.addTo(map);
+*/
+
+/* 54 Kitty Kat Club */
+var marker = new mapboxgl.Marker()
+.setLngLat([-93.236455, 44.980044])
 .addTo(map);
